b9c16267876f974cf9f5cd32dd4e345abf238f69
mary.vokicic@starfinanz.de <mary.vokicic@starfinanz.de>
2020-02-09 12:47:10 +0100

    add step 2

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <quiz :movies="movies"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Quiz from "./components/Quiz.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Quiz
+  },
+  data() {
+    return {
+      movies: [
+        "Harry Potter and the Philosopher's Stone",
+        "Harry Potter and the Chamber of Secrets",
+        "Harry Potter and the Prisoner of Azkaban",
+        "Harry Potter and the Goblet of Fire",
+        "Harry Potter and the Order of the Phoenix",
+        "Harry Potter and the Half-Blood Prince",
+        "Harry Potter and the Deathly Hallows - Part 1",
+        "Harry Potter and the Deathly Hallows - Part 2"
+      ]
+    };
   }
 }
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+html {
+  height: 100%;
+}
+body {
+  height: 100%;
+  background: #020815;
+  color: #eee;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/Quiz.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <img
+      src="https://media0.giphy.com/media/Bh3YfliwBZNwk/giphy.gif?cid=3640f6095c852266776c6f746fb2fc67"
+      alt
+    />
+    <h1 class="quiz-heading">How Well Do You Know the Harry Potter Movies?</h1>
+    <button class="quiz-button" v-if="stage==='welcome'" @click="initQuizStage">Start Quiz</button>
+    <ul class="quiz-choices" v-if="stage==='quiz'">
+      <li v-for="movie in movies" :key="movie">{{ movie }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    movies: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      currentQuestionNo: 0,
+    }
+  },
+  computed: {
+    stage() {
+        return !this.currentQuestionNo ? 'welcome' : 'quiz';
+    }
+  },
+  methods: {
+    initQuizStage() {
+      this.currentQuestionNo = 1;
+    }
+  }
+};
+</script>
+
+<style>
+.quiz-heading {
+  margin: -40px 0 30px;
+  font-size: 30px;
+  text-shadow: 1px 1px 2px #020815;
+  line-height: 1.2;
+}
+
+.quiz-button {
+  color: #eee;
+  text-decoration: none;
+  cursor: pointer;
+  display: inline-block;
+  padding: 10px 30px;
+  border: 1px solid rgba(238, 238, 238, 0.3);
+  background: none;
+  transition: border-color 0.5s;
+}
+.quiz-button:hover {
+  border-color: #eee;
+}
+</style>
